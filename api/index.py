3954caad7f029c878c94171309402e7f4f32a3fd
m801698 <145537227+m801698@users.noreply.github.com>
2023-11-17 09:22:19 +0100

    Update index.py

--- api/index.py
+++ api/index.py
@@ -1,10 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
     return 'Hello, World!'
+
+@app.route('/test')
+def test():
+    return render_template("test.html")
 
 @app.route('/about')
 def about():
